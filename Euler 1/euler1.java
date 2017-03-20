c7294586c559fe8a28e794dedfd1b0dbce7559df
Kai-Rey Lee <kairey@gmail.com>
2017-03-19 18:23:03 -0700

    Problem 1: Multiples of 3 and 5

---
+++ Euler 1/euler1.java
@@ -0,0 +1,11 @@
+public class Problem 1{
+    public static void main(String args[])  {
+        int ans=0;
+        for(int i=0; i<1000; i++) {
+            if(i%3 == 0 || i%5 == 0){
+                ans += i;
+            }
+        }
+        System.out.println(ans);
+    }
+}
